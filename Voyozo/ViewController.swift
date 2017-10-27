68ad7124254e1ffcfc91e02893173648a0bb2528
rex <rex@sangwooui-MacBook-Air.local>
2017-10-27 12:40:17 +0900

    Initial Commit

---
+++ Voyozo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voyozo
+//
+//  Created by rex on 2017. 10. 27..
+//  Copyright © 2017년 ijuyong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
